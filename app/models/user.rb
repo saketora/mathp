2482f8a113e39c3f7e891d836eb9534c7e8df031
Ubuntu <ubuntu@ip-172-31-83-64.ec2.internal>
2020-10-16 00:41:59 +0000

    give  up

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ has_secure_password
  validates :password, presence: true, length: { minimum: 6 }
  validates :class_name, presence: true, #format: { with: /[d]+[-]+[d]+[-]+[d]/ } ,
  length: { is: 8 }
+ 
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+
+end
 end
