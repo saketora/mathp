98122aa0be72c57027fedf700699d9a19080a6d3
Ubuntu <ubuntu@ip-172-31-83-64.ec2.internal>
2020-10-16 01:58:30 +0000

    mini

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
 require("bootstrap").start()
 //= require jquery
 //= require bootstrap
