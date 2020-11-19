2a58d9f726026ab7ae6ce77c3dce15f98faf232e
Ubuntu <ubuntu@ip-172-31-82-246.ec2.internal>
2020-11-19 23:20:45 +0000

    b

--- app/models/chapter1.rb
+++ app/models/chapter1.rb
@@ -1,7 +1,5 @@
 class Chapter1 < ApplicationRecord
      belongs_to :user
-     validates :course_id, presence: true
-　　 validates :chapter_id, presence: true
-　　 validates :section_id, presence: true
+     
      validates :user_id, presence: true , uniqueness: { scope: [ :course_id, :chapter_id , :section_id]  }
 end
